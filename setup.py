c916a93a77d30f3a9b1e4c67979def57ef8b9c72
John Wiggins <john.wiggins@xfel.eu>
2015-06-03 14:11:43 +0200

    Initial commit of main package and setup script.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+from distutils.core import setup
+
+import setuptools
+
+with open('README.md') as fp:
+    long_description = fp.read()
+
+setup(name='pr_demo',
+      version='0.0.1',
+      author='John Wiggins',
+      author_email='john.wiggins@xfel.eu',
+      description='Github-Flow Demonstration',
+      long_description=long_description,
+      url='https://github.com/jwiggins/PR-Demo',
+      packages=setuptools.find_packages(),
+      )
+
